b410f22066c6ebaf21d09d99187167959976177a
GerardBlasco <a24gerblaper@inspedralbes.cat>
2026-05-13 12:55:20 +0200

    Añadido MotionEvent Move a la funcion que ejecuta el sonido

--- app/src/main/java/com/example/irisfrades_gerardblasco_ra03_act02/MainActivity.kt
+++ app/src/main/java/com/example/irisfrades_gerardblasco_ra03_act02/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.irisfrades_gerardblasco_ra03_act02
 
+import android.annotation.SuppressLint
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,15 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        soundPool = SoundPool.Builder().setMaxStreams(1).build()
+        soundPool = SoundPool.Builder().setMaxStreams(2).build()
+
+        soundMap[R.id.c4] = soundPool.load(this, R.raw.faaa, 1)
+        soundMap[R.id.a3] = soundPool.load(this, R.raw.faaa, 1)
+        soundMap[R.id.b3] = soundPool.load(this, R.raw.faaa, 1)
+
+        loadKey(R.id.c4)
+        loadKey(R.id.a3)
+        loadKey(R.id.b3)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun loadKey(id: Int){
         val key = findViewById<ImageButton>(id)
 
         key.setOnTouchListener { v, event ->
             when(event.action){
-                MotionEvent.ACTION_DOWN -> {
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
                     val soundId = soundMap[id]
                     if(soundId != null){
                         soundPool.play(soundId, 1f, 1f, 0, 0, 1f)
